2b1cf344e7bb7e9cc87301751c6d006fc2ae2f35
Marija Petrovic <marry.sugar97@gmail.com>
2023-05-18 00:19:32 +0200

    Added signIn and singUp

--- UrediDomFront/src/components/Navbar.tsx
+++ UrediDomFront/src/components/Navbar.tsx
@@ -1,12 +1,23 @@
 import { FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.scss"
+import { useCookies } from "react-cookie";
+import Button from "./Button";
+import { useCallback } from "react";
 
 const Navbar = () => {
+  const [cookies, _setCookie, removeCookie] = useCookies(['token'])
+  const navigate = useNavigate();
+
+  const handleLogOut = useCallback(() => {
+    removeCookie("token")
+    navigate("/")
+  }, [navigate, removeCookie])
+
   return <div className="navbar">
     <div className="navbar__left">
       <Link to="/">
-        <img src="/logo.png" alt="logo" width="120px" height="120px"/>
+        <img src="/logo.png" alt="logo" width="80px" height="80px"/>
       </Link>
       <Link to="/productList">
         Products
@@ -16,12 +27,25 @@ const Navbar = () => {
       <Link to="/order">
         <FaShoppingCart /> Cart
       </Link>
-      <Link to="/signIn">
-        Sign in
-      </Link>
-      <Link to="signUp">
-        Sign up
-      </Link>
+      {cookies.token ?
+      <>
+        <Link to="/profile">
+          Profile
+        </Link>
+        <Button onClick={handleLogOut}>
+          Log out
+        </Button>
+      </>
+      :
+      <>
+        <Link to="/signIn">
+          Sign in
+        </Link>
+        <Link to="signUp">
+          Sign up
+        </Link>
+      </>
+      }
     </div>
   </div>
 }
